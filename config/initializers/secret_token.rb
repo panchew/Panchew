25a96be8c60a7684f80df7e7846263a346f1e8a0
Francisco Guzman <francisco.guzman@crowdint.com>
2010-11-18 14:23:42 -0600

    rails app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weallwin::Application.config.secret_token = '98161145e951daade7db577777fe02d1c01fa9987571b593a7e214d80bbad08b430b7202fe1c0335b063955ccc53066a1128f3415f2dc4453086f650c9b418ef'
